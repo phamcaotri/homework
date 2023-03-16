845b8e897bdd60d648ffb9b304fab5093cceeabe
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-16 10:58:15 +0700

    chỉnh sửa saa

---
+++ SAA/P1/binarysearch.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void input(int a[], int n) {
+    for (int i = 0; i< n; i++) {
+        cin >> a[i];
+    }
+}
+
+void output(int a[], int n) {
+    for (int i = 0; i<n ; i++) {
+        cout << a[i];
+    }
+}
+
+int BinarySearch(int a[], int x, int start, int end) {
+    //tìm điểm nằm giữa
+    if (end < start)
+        return -1;
+    int mid  =  (end+start)/2;
+
+    if (x == a[mid])
+        return mid;
+    else {
+        if (x > a[mid])
+            return BinarySearch(a,x,mid+1,end);
+        else
+            return BinarySearch(a,x,start,mid-1);
+    }
+    
+    // 
+}
+int main() {
+    int n, m;
+    cin >> n;
+    int a[100], x[100];
+    input(a,n);
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> x[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cout << BinarySearch(a,x[i],0,n-1) << '\n';
+    }
+
+    
+
+    return 0;
+}
---
+++ SAA/P1/test.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+using namespace std;
+
+void inputArray(int a[],int &n)
+{
+    
+    cin >> n;
+    
+    
+    int *b = new int[n];
+    a = b;
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+}
+
+
+void findElements(int a[], int n, int m)
+{
+    bool is_find = false;
+    cin >> m;
+    int *x = new int[m];
+    for (int i = 0; i < m; i++) {
+        cin >> x[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[j] == x[i])
+            {
+                cout << j << '\n';
+                is_find = true;
+                break;
+            } 
+               
+             
+        }
+        if (not is_find)
+            cout << -1 << '\n';
+        else
+            is_find = false;
+    }
+}
+
+int main()
+{
+    int n,m;
+    int *a=NULL;
+    inputArray(a,n);
+    findElements(a,n,m);
+
+    return 0;
+}
