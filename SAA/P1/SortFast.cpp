c7b83fd5434625417b8a717e85fe8c919ef73d00
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-17 13:35:46 +0700

    update SortFast.cpp, SortFast.exe and tempCodeRunnerFile.cpp

--- SAA/P1/SortFast.cpp
+++ SAA/P1/SortFast.cpp
@@ -1,5 +1,57 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int max(int a[], int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+void draw(int a[], int n, int max_value, int pivot, int i, int j) {
+    
+    string buffer;
+    system("cls");
+    for (int k = max_value; k > 0; k--) {
+        for (int l = 0; l < n; l++) {
+            if (a[l] >= k) {
+                if (l == pivot) {
+                    if (a[l] < 10)
+                        buffer += "\033[32m" + to_string(a[l]) + " " + "\033[0m";
+                    else
+                        buffer += "\033[32m" + to_string(a[l]) + "\033[0m";
+                }
+                else if (l == i || l == j) {
+                    if (a[l] < 10)
+                        buffer += "\033[31m" + to_string(a[l]) + " " + "\033[0m";
+                    else
+                        buffer += "\033[31m" + to_string(a[l]) + "\033[0m";
+                }
+                else {
+                    if (a[l] < 10)
+                        buffer += to_string(a[l]) + " ";
+                    else
+                        buffer += to_string(a[l]);
+                }
+            }
+            else {
+                buffer.append("  ");
+            }
+
+            buffer.append(" ");
+        }
+        buffer.append("\n");
+
+    }
+    for (int m = 0; m < n; m++) {
+        buffer.append("---");
+    }
+    cout << buffer;
+    this_thread::sleep_for(std::chrono::milliseconds(100));
+}
 
 void input(int a[], int n) {
     for (int i = 0; i < n; i++)
@@ -12,29 +64,33 @@ void output(int a[], int n) {
     }
 }
 
-void QuickSort(int a[], int left, int right) {
+void QuickSort(int a[], int n ,int left, int right) {
+    int max_value = max(a,n);
     int mid = a[(left+right)/2];
     int i = left, j = right;
-
+    draw(a,n, max_value,(left+right)/2,i,j);
     while (i < j) {
         while (a[i] < mid) {
             i ++;
+            draw(a,n, max_value,(left+right)/2,i,j);
         }
 
         while (a[j] > mid) {
             j --;  
+            draw(a,n, max_value,(left+right)/2,i,j);
         }
 
         if (i <= j) {
             swap(a[i], a[j]);
+            draw(a,n,max_value,(left+right)/2,i,j);
               
             i ++; j --;
         }
         
     }
     
-    if (left < j) QuickSort(a,left,j);
-    if (right > i) QuickSort(a,i,right);
+    if (left < j) QuickSort(a,n ,left,j);
+    if (right > i) QuickSort(a,n ,i,right);
 }
 
 int main() {
@@ -42,7 +98,8 @@ int main() {
     cin >> n;
     int *a = new int[n];
     input(a,n);
-    QuickSort(a,0,n-1);
-    output(a,n);
+    QuickSort(a,n,0,n-1);
+    //output(a,n);
+    cin >> n;
     return 0;
 }
--- SAA/P1/tempCodeRunnerFile.cpp
+++ SAA/P1/tempCodeRunnerFile.cpp
@@ -1,2 +1,2 @@
 
-        if (i <= j) {
+                        buffer += "\033[31m" + to_string(a[l]) + " " + "\033[0m";
