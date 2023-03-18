04a222ffd736c6c12da55a417d4f09b0fe273337
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-18 11:18:42 +0700

    update 10 files

--- SAA/P1/SortFastWithOOP.cpp
+++ SAA/P1/SortFastWithOOP.cpp
@@ -8,30 +8,10 @@ int main() {
     int n;
     cin >> n;
     int *a = new int[n];
-    /*
-    SimpleSort SimpleSort;
-    SimpleSort.input(a,n);
-    SimpleSort.setDelayTime(0);
-    SimpleSort.SortAndDraw(a,n);
-
-    BubbleSort BubbleSort;
-    BubbleSort.input(a,n);
-    BubbleSort.setDelayTime(0);
-    BubbleSort.SortAndDraw(a,n);
-    */
-    QuickSort QuickSort1, QuickSort2;
-    QuickSort1.input(a,n);
-    QuickSort1.setDelayTime(10);
-    QuickSort1.SortAndDraw2(a,n,0,n-1);
-    QuickSort2.input(a,n);
-    QuickSort2.setDelayTime(10);
-    QuickSort2.SortAndDraw(a,n,0,n-1);
-
-
-    MergeSort MergeSort;
-    MergeSort.input(a,n);
-    MergeSort.setDelayTime(50);
-    MergeSort.SortAndDraw(a,n,0,n-1);
+    
+    QuickSort qs1(a,n,10);
+    qs1.createRamdomizeArray();
+    qs1.SortAndDraw(0,n-1);
 
     return 0;
 }
