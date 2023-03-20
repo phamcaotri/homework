e91328704efbd3eb1de71068f57af6c11d09c349
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-20 13:25:32 +0700

    update 9 files

--- SAA/P1/SortFastWithOOP.cpp
+++ SAA/P1/SortFastWithOOP.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int main() {
     int n = 50;
-    //cin >> n;
-    // vector<int> a(n);
-    
+
     //s1.SortAndDraw_RandomPivot(0,n-1);
     //for (int i = 10; i = 1000000000; i *= 10) {
-        BubbleSort s1(n,10);
+        MergeSort s1(n,100);
         s1.createShuffledArray();
         //auto start = high_resolution_clock::now();
-        s1.SortAndDraw();
+        s1.SortAndDraw(0,n-1);
         //auto stop = high_resolution_clock::now();
         //auto duration = duration_cast<nanoseconds>(stop - start);
         //cout << i << ", runtime: " << duration.count() << " ns" << '\n';
