1f634b19efb13e4c2c911ae1906fa168bb0a9625
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-22 08:34:13 +0700

    update BubbleSort.h, ShakeSort.h, SortFastWithOOP.cpp and SortFastWithOOP.exe

---
+++ BaitapOOP/P2/PhanSo.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class PhanSo {
+    private:
+        int tu_so;
+        int mau_so;
+    public:
+        PhanSo() {}
+        PhanSo(int tu_so, int mau_so) : tu_so(tu_so) , mau_so(mau_so) {}
+        void setPhanSo() {
+            cin >> tu_so;
+            cin >> mau_so;
+            
+
+        }
+        int getTuSo() {
+            return tu_so;
+        }
+
+        int getMauSo() {
+            return mau_so;
+        }
+
+        float getValuePhanSo() {
+            if (mau_so != 0)
+                return (float) tu_so/mau_so;
+        }
+    PhanSo Cong_PhanSo(PhanSo B) {
+        return {(tu_so*B.getMauSo()+B.getTuSo()*mau_so),(mau_so*B.getMauSo())};
+    }
+
+    float Cong(PhanSo B) {
+        return (float) (tu_so*B.getMauSo()+B.getTuSo()*mau_so)/(mau_so*B.getMauSo());
+    }
+    float Cong_Cach2(PhanSo B) {
+        return (float) this -> getValuePhanSo() + B.getValuePhanSo();
+    }
+
+};
+
+
+
+
+int main() {
+    PhanSo A, B;
+    A.setPhanSo();
+    B.setPhanSo();
+    cout << "Tong2 = " << A.Cong_Cach2(B) << '\n';
+    cout << "Tong1 = " << A.Cong(B) << '\n';
+    cout << A.Cong_PhanSo(B).getTuSo() << '/' << A.Cong_PhanSo(B).getMauSo();
+
+    return  0;
+}
+
