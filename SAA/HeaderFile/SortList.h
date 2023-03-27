53c659543f9b113329b578b3918eb49a38fed85b
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-27 21:30:49 +0700

    update ArrayFunction.h

--- SAA/HeaderFile/SortList.h
+++ SAA/HeaderFile/SortList.h
@@ -4,20 +4,8 @@
 #include "SortIndex.h"
 #include "ArrayFunction.h"
 class SortList {
-private:
-    vector<void*> algorithms;
-public:
-    void addAlgorithm(void* algorithm) {
-        algorithms.push_back(algorithm);
-    }
-    void runAllAlgorithms(int n) {
-        for (auto algorithm : algorithms) {
-            algorithm->setN(n);
-            algorithm->createTestArray();
-            algorithm->measureTime();
-            algorithm->isSorted();
-        }
-    }
+    
+
 };
 
 #endif
