07dd20917babc6d5da30c527d515da9dd62dccee
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-30 15:06:04 +0700

    update Run.cpp and Run.exe

--- BaiTapDSA/Run.cpp
+++ BaiTapDSA/Run.cpp
@@ -5,16 +5,16 @@ int main() {
 
     vector<SortAlgorithm*> algorithms;
     //algorithms.push_back(new BubbleSort);
-        // algorithms.push_back(new CountSort);
-        // algorithms.push_back(new HeapSort);
+        algorithms.push_back(new CountSort);
+        algorithms.push_back(new HeapSort);
     //algorithms.push_back(new BinaryInsertionSort);
     //algorithms.push_back(new InsertionSort);
     //algorithms.push_back(new InterchangeSort);
-        // algorithms.push_back(new MergeSort);
-        //algorithms.push_back(new QuickSort);
+        algorithms.push_back(new MergeSort);
+        algorithms.push_back(new QuickSort);
     //algorithms.push_back(new SelectionSort);
     //algorithms.push_back(new ShakeSort);
-        // algorithms.push_back(new ShellSort);
+        algorithms.push_back(new ShellSort);
         algorithms.push_back(new RadixSort);
     SortList list;
 // add algorithms to list
