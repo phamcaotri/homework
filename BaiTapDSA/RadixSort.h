cfeddd9c57df939149926bb93ae2703f793ff32d
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-31 08:41:18 +0700

    update 8 files

--- BaiTapDSA/RadixSort.h
+++ BaiTapDSA/RadixSort.h
@@ -15,35 +15,35 @@ class RadixSort : public SortAlgorithm {
         string getName() {
             return "Radix Sort";
         }
-        
-        // radix sort, using vector a, n, max_value, min_value from class,  sort byte by byte, and works with negative numbers
+
         void Sort() {
-            // if there are negative numbers, shift all numbers to positive
+            // nếu có số âm, thì chuyển tất cả các số về dương
             if (min_value < 0) {
                 for (int i = 0; i < n; i++) {
                     a[i] -= min_value;
                 }
                 max_value -= min_value;
             }
+            // tính số chữ số của số lớn nhất
             int max_digit = to_string(abs(max_value)).size();
-            int base = 1;
-            for (int i = 0; i < max_digit; i++) {
-                vector<int> b(n);
-                vector<int> c(10,0);
-                for (int j = 0; j < n; j++) {
-                    c[(a[j]/base)%10]++;
+            
+            vector<vector<int>> b(10,vector<int> (n)); // mảng b để lưu các phần tử
+            for (int base = 1; base < pow(10,max_digit); base *= 10) {
+                vector<int> c(10,0); // mảng c để lưu số lần xuất hiện của các phần tử
+                for (int i = 0; i < n; i++) {
+                    int index = (a[i]/(base))%10;
+                    b[index][c[index]] = a[i]; // chèn phần tử trong mảng a vào mảng b
+                    c[index] ++;
                 }
-                for (int j = 1; j < 10; j++) {
-                    c[j] += c[j-1];
+                int k = 0;
+                for (int i = 0; i < 10; i++) {
+                    for (int j = 0; j < c[i]; j++ ) {
+                        a[k] = b[i][j]; // chèn kết quả lại mảng a
+                        k++;
+                    }
                 }
-                for (int j = n-1; j >= 0; j--) {
-                    b[c[(a[j]/base)%10] - 1] = a[j];
-                    c[(a[j]/base)%10]--;
-                }
-                a = b;
-                base *= 10;
             }
-            // if there are negative numbers, shift all numbers back to negative
+            // nếu có số âm, thì chuyển về lại
             if (min_value < 0) {
                 for (int i = 0; i < n; i++) {
                     a[i] += min_value;
--- BaiTapDSA/ShakeSort.h
+++ BaiTapDSA/ShakeSort.h
@@ -18,22 +18,22 @@ class ShakeSort : public SortAlgorithm {
 
             for (int i = 0; i < n/2 ; i++) {
                 swapped = false;
-                for (int j = i; j < n-i-1; j++) {
+                for (int j = i; j < n-i-1; j++) { // chạy từ đầu đến cuối
                     
-                    if (a[j] > a[j+1]) {
-                        swap(a[j],a[j+1]);
+                    if (a[j] > a[j+1]) {    // đổi chỗ 2 phần tử nếu sai thứ tự
+                        swap(a[j],a[j+1]); 
                         swapped = true;
                     }
                 }
 
 
-                for (int j = n-i-1 ; j > i ; j--) {
-                    if (a[j] < a[j-1]) {
+                for (int j = n-i-1 ; j > i ; j--) { // chạy từ cuối đến đầu
+                    if (a[j] < a[j-1]) {    // đổi chỗ 2 phần tử nếu sai thứ tự
                         swap(a[j],a[j-1]);
                         swapped = true;
                     }
                 }
-                if (not swapped)
+                if (not swapped) // nếu mảng đã được sắp xếp thì dừng
                     break;
 
             }
