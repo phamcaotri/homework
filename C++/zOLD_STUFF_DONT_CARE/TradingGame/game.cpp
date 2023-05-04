a803320586502fabe3cbce361c92b3f969462e69
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-05-04 17:51:50 +0700

    update 5 files

--- C++/zOLD_STUFF_DONT_CARE/TradingGame/game.cpp
+++ C++/zOLD_STUFF_DONT_CARE/TradingGame/game.cpp
@@ -26,14 +26,13 @@ class đọc input từ người dùng
 thêm mua bán số lượng lớn
 gom chung cha các class character, shop, thành một class entity
 fix vấn đề làm tròn số
+cơ chế tăng exp khi mua bán
 */
 /*--------------------------- TODO --------------------------------
 
-cơ chế tăng exp khi mua bán
-cơ chế lưu game
 cơ chế xem thông tin item
 cơ chế đọc file từ file lưu game.
 xây dựng một môi trường game cơ bản
 Thêm mô tả cho player
-
+cơ chế lưu game
 */
