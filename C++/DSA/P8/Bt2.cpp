ad4d4dd78462651479e7fb47c0e69ff27159c853
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-05-26 00:16:44 +0700

    delete 1 file and update 7 files

--- C++/DSA/P8/Bt2.cpp
+++ C++/DSA/P8/Bt2.cpp
@@ -7,21 +7,21 @@ using
 #include<iostream>
 using namespace std;
 
-void inputGraph(bool* [], int);
-void process(bool* [], int, int);
+void inputGraph(bool*[], int);
+void process(bool*[], int, int);
 
 
 
 int main()
 {
-    int v, e, n; //v: số đỉnh, e: số cạnh, n: số thao tác
-    cin >> v >> e >> n;
+	int v, e, n; //v: số đỉnh, e: số cạnh, n: số thao tác
+	cin >> v >> e >> n;
 
-    bool** G; // ma trận toàn số 0, 1 nên kiểu bool hay int đều được
+	bool **G; // ma trận toàn số 0, 1 nên kiểu bool hay int đều được
 
 
-    //###INSERT CODE HERE -
-    G = new bool* [v];
+//###INSERT CODE HERE -
+    G = new bool*[v];
     for (int i = 0; i < v; i++) {
         G[i] = new bool[v];
     }
@@ -39,8 +39,8 @@ int main()
 void inputGraph(bool* g[], int e) {
     for (int i = 0; i < e; i++) {
         int x, y; cin >> x >> y;
-        g[x - 1][y - 1] = 1;
-        g[y - 1][x - 1] = 1;
+        g[x-1][y-1] = 1;
+        g[y-1][x-1] = 1;
     }
 }
 
@@ -50,25 +50,24 @@ void process(bool* g[], int n, int v) {
         switch (num)
         {
         case 1:
-        {
+            {
             int u, i; cin >> u >> i;
-            if (g[u - 1][i - 1] == 1) {
+            if (g[u-1][i-1] == 1) {
                 cout << "TRUE";
-            }
-            else {
+            } else {
                 cout << "FALSE";
             }
             break;
-        }
-
+            }
+        
         default:
-        {
+            {
             int u;
             cin >> u;
             bool flag = false;
             for (int i = 0; i < v; i++) {
-                if (g[u - 1][i] == 1) {
-                    cout << i + 1 << " ";
+                if (g[u-1][i] == 1) {
+                    cout << i+1 << " ";
                     flag = true;
                 }
             }
@@ -76,7 +75,7 @@ void process(bool* g[], int n, int v) {
                 cout << "NONE";
             }
             break;
-        }
+            }
         }
         cout << '\n';
     }
