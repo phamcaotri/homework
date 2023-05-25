51a63da812443b639cf29b62f5f64bbd608895a4
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-05-26 00:30:13 +0700

    update Bt5.cpp and Dothi.cpp

--- C++/DSA/P8/Dothi.cpp
+++ C++/DSA/P8/Dothi.cpp
@@ -7,8 +7,12 @@ int main() {
     for (int i = 0; i < n; i++) {
         a[i] = new int[n];
     }
-
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    
 
     for (int i = 0; i < n; i++) {
         delete[] a[i];
