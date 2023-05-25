327dda4a4c89a8a7be43c1748e5165a9fe134088
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-05-25 16:31:59 +0700

    update note.txt and smartphone2.cpp

---
+++ C++/ThucHanhOOP/Tuan6/QLPhong.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class QuanLyPhong {
+    pub
+};
+
+int main() {
+    QuanLyPhong phong;
+    phong.Nhap();
+    phong.Xuat();
+    return 0;
+}
