a35982ffce4e643fbf432bbca0a2c78324851c1a
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-06-11 18:40:40 +0700

    update main.cpp, main.exe, main_data.txt and unit_test_writefile.txt

--- C++/zOLD_STUFF_DONT_CARE/AnotherGame/main.cpp
+++ C++/zOLD_STUFF_DONT_CARE/AnotherGame/main.cpp
@@ -9,38 +9,27 @@
 #include <thread>
 #include <utility>
 #include <map>
+
 using namespace std;
 
 const string MAIN_DATA_FILE = "main_data.txt";
 
-
 class FileReader {
     protected:
         ifstream file;
-        int pos = 0;
     public:
         FileReader(string file_name) {
             file.open(file_name);
+            if (!file.is_open()) {
+                cout << "Error opening file: " << file_name << endl;
+                exit(1);
+            }
         }
         ~FileReader() {
             file.close();
         }
         virtual string read() = 0;
 
-        streampos get_pos() {
-            return pos;
-        }
-        void set_pos(int pos) {
-            this->pos = pos;
-        }
-        void reset_pos() {
-            pos = 0;
-        }
-        streampos go_to_string(string str) {
-            
-        }
-        streampos go_to_line(int line) {
-        }
 };
 
 class ReadAll : public FileReader {
@@ -92,8 +81,6 @@ class ReadUntil : public FileReader {
         }
 };
 
-
-
 class FileWriter {
     private:
         ofstream file;
@@ -101,142 +88,17 @@ class FileWriter {
     public:
         FileWriter(string file_name) {
             this->file_name = file_name;
+            file.open(file_name);
         }
         ~FileWriter() {
             file.close();
         }
         void write(string data) {
-            file.open(file_name);
             file << data;
         }
 };
 
-#define GREEN(x) "\033[32m" x "\033[0m"
-#define RED(x) "\033[31m" x "\033[0m"
-#define BLUE(x) "\033[34m" x "\033[0m"
-#define YELLOW(x) "\033[33m" x "\033[0m"
-#define MAGENTA(x) "\033[35m" x "\033[0m"
-#define CYAN(x) "\033[36m" x "\033[0m"
 
-typedef function<bool(string&)> TestFunc;;
-class Test {
-    private:
-        vector<pair<string, TestFunc>> tests;
-        int passed = 0;
-        int failed = 0;
-    public:
-        void init() {
-            passed = 0;
-            failed = 0;
-            tests.clear();
-        }
-        void add_list(vector<pair<string, TestFunc>>& list) {
-            for (int i = 0; i < list.size(); i++) {
-                tests.push_back(list[i]);
-            }
-        }
-        void run() {
-            vector<pair<string, string>> failed_tests;
-            cout << "===============================" << endl;
-            for (int i = 0; i < tests.size(); i++) {
-                auto [name, func] = tests[i];
-                string error_msg = "";
-
-                cout << "[RUN TEST " << i + 1 << "/" << tests.size() << "] ";
-                auto start = chrono::high_resolution_clock::now();
-                if ((func)(error_msg)) {
-                    auto end = chrono::high_resolution_clock::now();
-                    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-                    cout << GREEN("[PASSED] ") << " " << name << " " << duration.count() << " ms" << endl;
-                    passed++;
-                } else {
-
-                    cout << RED("[FAILED] ") << " " << name << endl;
-                    failed_tests.push_back({name, error_msg});
-                    failed++;
-                }
-            }
-            cout << "===============================" << endl;
-            cout << "FAILED TESTS: " << failed_tests.size() << endl;
-            for (int i = 0; i < failed_tests.size(); i++) {
-                auto [name, error_msg] = failed_tests[i];
-                cout << RED("[FAILED] ") << " " << name << endl;
-                cout << error_msg << endl;
-            }
-
-            cout << "===============================" << endl;
-            cout << "TOTAL TESTS: " << tests.size() << endl;
-            cout << GREEN("PASSED: ") << passed << endl;
-            cout << RED("FAILED: ") << failed << endl;
-            cout << "===============================" << endl;
-        }
-};
-
-const string UNIT_TEST_READFILE = "unit_test_readfile.txt";
-const string UNIT_TEST_WRITEFILE = "unit_test_writefile.txt";
-
-
-bool unit_test_readall(string& msg) {
-    string expected = "THIS IS LINE 1\nthis is line 3";
-    ReadAll reader(UNIT_TEST_READFILE);
-    string data = reader.read();
-    if (data == expected) {
-        return true;
-    }
-    msg = "Expected:\n" + expected + "\nGot:\n" + data; 
-    msg += "\nFile: " + UNIT_TEST_READFILE;
-    return false;
-}
-
-bool unit_test_readline(string& msg) {
-    string expected = "THIS IS LINE 1";
-    ReadLine reader(UNIT_TEST_READFILE);
-    string data = reader.read();
-    if (data == expected) {
-        return true;
-    }
-    msg = "Expected: " + expected + "\nGot: " + data;
-    msg += "\nFile: " + UNIT_TEST_READFILE;
-    return false;
-}
-
-bool unit_test_readuntil(string& msg) {
-    string expected = "THIS IS LINE ";
-    ReadUntil reader(UNIT_TEST_READFILE, '1');
-    string data = reader.read();
-    if (data == expected) {
-        return true;
-    }
-    msg = "Expected: " + expected + "\nGot: " + data;
-    msg += "\nFile: " + UNIT_TEST_READFILE;
-    return false;
-}
-bool unit_test_writefile(string& msg) {
-    string expected = "THIS IS LINE 1\nthis is line 3";
-    {
-        FileWriter writer(UNIT_TEST_WRITEFILE);
-        writer.write("THIS IS LINE 1\n");
-        writer.write("# THIS IS LINE 2\n");
-        writer.write("this is line 3");
-    }
-    ReadAll reader(UNIT_TEST_WRITEFILE);
-    string data = reader.read();
-    if (data == expected) {
-        FileWriter writer(UNIT_TEST_WRITEFILE);
-        writer.write("");
-        return true;
-    }
-    msg = "Expected:\n" + expected + "\nGot:\n" + data; 
-    msg += "\nFile: " + UNIT_TEST_WRITEFILE;
-    return false;
-}
-
-vector<pair<string, TestFunc>> test_funcs = {
-    {"ReadAll", unit_test_readall},
-    {"ReadLine", unit_test_readline},
-    {"ReadUntil", unit_test_readuntil},
-    {"WriteFile", unit_test_writefile},
-};
 
 class BasicInfo {
     private:
@@ -331,49 +193,86 @@ class Skill {
         }
 };
 
+class DataProcess {
+    private:
+        string data;
+        int pos;
+    public:
+        DataProcess() {
+            data = "";
+            pos = 0;
+        }
+        DataProcess(string data) {
+            this->data = data;
+            pos = 0;
+        }
+        void set_data(string data) {
+            this->data = data;
+        }
+        void reset_pos() {
+            pos = 0;
+        }
+        string getline() {
+            string line = "";
+            while (data[pos] != '\n' && pos < data.length()) {
+                line += data[pos];
+                pos++;
+            }
+            pos++;
+            return line;
+        }
+        // this brings pos to the end of the given string, or the end of the data if the string is not found
+        void go_to_string(string str) {
+            pos = data.find(str);
+            if (pos == string::npos) {
+                pos = data.length();
+                return;
+            }
+            pos += str.length();
+        }
+
+};
+
 class GameData {
     private:
-        string file_name;
-        ReadUntil reader;
-        //FileWriter writer;
+        DataProcess data;
     public:
-        GameData(string file_name) : reader(file_name, '\n') {
-            this->file_name = file_name;
+        GameData(string file_name) {
+            ReadAll reader(file_name);
+            data.set_data(reader.read());
         }
         BasicInfo get_player_basic_info() {
-            streampos pos = reader.go_to_string("PLAYER_INFO");
-            string name = reader.read(pos);
-            string description = reader.read(pos);
-            string image = reader.read(pos);
+            data.reset_pos();
+            data.go_to_string("PLAYER_INFO\n");
+            string name = data.getline();
+            string description = data.getline();
+            string image = data.getline(); 
             return BasicInfo(name, description, image);
         }
         StastInfo get_player_stats_info() {
-            streampos pos = reader.go_to_string("PLAYER_STATS");
-            int hp = stoi(reader.read(pos));
-            int mp = stoi(reader.read(pos));
-            int attack = stoi(reader.read(pos));
-            int defense = stoi(reader.read(pos));
-            int speed = stoi(reader.read(pos));
+            data.reset_pos();
+            data.go_to_string("PLAYER_STATS\n");
+            int hp = stoi(data.getline());
+            int mp = stoi(data.getline());
+            int attack = stoi(data.getline());
+            int defense = stoi(data.getline());
+            int speed = stoi(data.getline());
             return StastInfo(hp, mp, attack, defense, speed);
         }        
         vector<Skill> get_player_skills() {
-            ReadUntil reader(file_name, '\n');
-            streampos pos = reader.go_to_string("PLAYER_SKILLS");
+            data.reset_pos();
+            data.go_to_string("PLAYER_SKILLS\n");
             vector<Skill> skills;
-            string name = reader.read(pos);
-            string description = reader.read(pos);
-            string image = reader.read(pos);
-            int skill_count = stoi(reader.read(pos));
+            int skill_count = stoi(data.getline());
             for (int i = 0; i < skill_count; i++) {
-                string name = reader.read(pos);
-                string description = reader.read(pos);
-                int damage = stoi(reader.read(pos));
-                int mp_cost = stoi(reader.read(pos));
+                string name = data.getline();
+                string description = data.getline();
+                int damage = stoi(data.getline());
+                int mp_cost = stoi(data.getline());
                 skills.push_back(Skill(name, description, damage, mp_cost));
             }
             return skills;
         }
-
 };
 
 class Character {
@@ -619,44 +518,44 @@ class Enemy : public Character {
 };
 
 class CharacterFactory {
-    // private:
-    //     GameData game_data;
-    // public:
-    //     CharacterFactory(GameData game_data) {
-    //         this->game_data = game_data;
-    //     }
-    //     Player create_player() {
+        // private:
+        //     GameData game_data;
+        // public:
+        //     CharacterFactory(GameData game_data) {
+        //         this->game_data = game_data;
+        //     }
+        //     Player create_player() {
 
-    //     }
-    //     Enemy create_enemy() {
+        //     }
+        //     Enemy create_enemy() {
 
-    //     }
+        //     }
 };
 
 class SkillFactory {
-    // private:
-    //     GameData game_data;
-    // public:
-    //     SkillFactory(GameData game_data) {
-    //         this->game_data = game_data;
-    //     }
-    //     Skill create_skill(string skill_name) {
+        // private:
+        //     GameData game_data;
+        // public:
+        //     SkillFactory(GameData game_data) {
+        //         this->game_data = game_data;
+        //     }
+        //     Skill create_skill(string skill_name) {
 
-    //     }
+        //     }
 };
 
 class Battle {
-    // private:
-    //     Player player;
-    //     vector<Enemy> enemies;
-    // public:
-    //     Battle(Player player, vector<Enemy> enemies) {
-    //         this->player = player;
-    //         this->enemies = enemies;
-    //     }
-    //     void run() {
+        // private:
+        //     Player player;
+        //     vector<Enemy> enemies;
+        // public:
+        //     Battle(Player player, vector<Enemy> enemies) {
+        //         this->player = player;
+        //         this->enemies = enemies;
+        //     }
+        //     void run() {
 
-    //     }
+        //     }
 };
 
 class Game {
@@ -673,6 +572,149 @@ class Game {
         void run() {
 
         }
+};
+
+
+#define GREEN(x) "\033[32m" x "\033[0m"
+#define RED(x) "\033[31m" x "\033[0m"
+#define BLUE(x) "\033[34m" x "\033[0m"
+#define YELLOW(x) "\033[33m" x "\033[0m"
+#define MAGENTA(x) "\033[35m" x "\033[0m"
+#define CYAN(x) "\033[36m" x "\033[0m"
+
+typedef function<bool(string&)> TestFunc;;
+class Test {
+    private:
+        vector<pair<string, TestFunc>> tests;
+        int passed = 0;
+        int failed = 0;
+    public:
+        void init() {
+            passed = 0;
+            failed = 0;
+            tests.clear();
+        }
+        void add_list(vector<pair<string, TestFunc>>& list) {
+            for (int i = 0; i < list.size(); i++) {
+                tests.push_back(list[i]);
+            }
+        }
+        void run() {
+            vector<pair<string, string>> failed_tests;
+            cout << "===============================" << endl;
+            for (int i = 0; i < tests.size(); i++) {
+                auto [name, func] = tests[i];
+                string error_msg = "";
+
+                cout << "[RUN TEST " << i + 1 << "/" << tests.size() << "] ";
+                auto start = chrono::high_resolution_clock::now();
+                if ((func)(error_msg)) {
+                    auto end = chrono::high_resolution_clock::now();
+                    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+                    cout << GREEN("[PASSED] ") << " " << name << " " << duration.count() << " ms" << endl;
+                    passed++;
+                } else {
+
+                    cout << RED("[FAILED] ") << " " << name << endl;
+                    failed_tests.push_back({name, error_msg});
+                    failed++;
+                }
+            }
+            cout << "===============================" << endl;
+            cout << "FAILED TESTS: " << failed_tests.size() << endl;
+            for (int i = 0; i < failed_tests.size(); i++) {
+                auto [name, error_msg] = failed_tests[i];
+                cout << RED("[FAILED] ") << " " << name << endl;
+                cout << error_msg << endl;
+            }
+
+            cout << "===============================" << endl;
+            cout << "TOTAL TESTS: " << tests.size() << endl;
+            cout << GREEN("PASSED: ") << passed << endl;
+            cout << RED("FAILED: ") << failed << endl;
+            cout << "===============================" << endl;
+        }
+};
+
+const string UNIT_TEST_READFILE = "unit_test_readfile.txt";
+const string UNIT_TEST_WRITEFILE = "unit_test_writefile.txt";
+
+
+bool unit_test_readall(string& msg) {
+    string expected = "THIS IS LINE 1\nthis is line 3";
+    ReadAll reader(UNIT_TEST_READFILE);
+    string data = reader.read();
+    if (data == expected) {
+        return true;
+    }
+    msg = "Expected:\n" + expected + "\nGot:\n" + data;
+    msg += "\nFile: " + UNIT_TEST_READFILE;
+    return false;
+}
+
+bool unit_test_readline(string& msg) {
+    string expected = "THIS IS LINE 1";
+    ReadLine reader(UNIT_TEST_READFILE);
+    string data = reader.read();
+    if (data == expected) {
+        return true;
+    }
+    msg = "Expected: " + expected + "\nGot: " + data;
+    msg += "\nFile: " + UNIT_TEST_READFILE;
+    return false;
+}
+
+bool unit_test_readuntil(string& msg) {
+    string expected = "THIS IS LINE ";
+    ReadUntil reader(UNIT_TEST_READFILE, '1');
+    string data = reader.read();
+    if (data == expected) {
+        return true;
+    }
+    msg = "Expected: " + expected + "\nGot: " + data;
+    msg += "\nFile: " + UNIT_TEST_READFILE;
+    return false;
+}
+bool unit_test_writefile(string& msg) {
+    string expected = "THIS IS LINE 1\nthis is line 3";
+    {
+        FileWriter writer(UNIT_TEST_WRITEFILE);
+        writer.write("THIS IS LINE 1\n");
+        writer.write("# THIS IS LINE 2\n");
+        writer.write("this is line 3");
+    }
+    ReadAll reader(UNIT_TEST_WRITEFILE);
+    string data = reader.read();
+    if (data == expected) {
+        FileWriter writer(UNIT_TEST_WRITEFILE);
+        writer.write("");
+        return true;
+    }
+    msg = "Expected:\n" + expected + "\nGot:\n" + data;
+    msg += "\nFile: " + UNIT_TEST_WRITEFILE;
+    return false;
+}
+
+bool check_main_data_valid(string& msg) {
+    ReadAll reader(MAIN_DATA_FILE);
+    string data = reader.read();
+    if (data.size() == 0) {
+        msg = "File is empty";
+        return false;
+    }
+    if (data.find("PLAYER_INFO") == string::npos || data.find("PLAYER_STATS") == string::npos || data.find("PLAYER_SKILLS") == string::npos) {
+        msg = "File does not contain valid player data";
+        return false;
+    }
+    return true;
+}
+
+vector<pair<string, TestFunc>> test_funcs = {
+        {"ReadAll", unit_test_readall},
+        {"ReadLine", unit_test_readline},
+        {"ReadUntil", unit_test_readuntil},
+        {"WriteFile", unit_test_writefile},
+        {"CheckMainDataValid", check_main_data_valid},
 };
 
 int main() {
