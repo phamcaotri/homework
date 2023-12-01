10dfa59959b850a3f868c81674699f26b7dd1ce5
phamcaotri <22521525@gm.uit.edu.vn>
2023-12-01 14:11:44 +0000

    Dec 1, 2023, 9:11 PM

--- C++/grapth.cpp
+++ C++/grapth.cpp
@@ -1,32 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    vector<int> bfs(map<int, set<int>> &graph, int start, vector<int> &k){
-        set<int> setK;
-        for (auto &i: k){
-            setK.insert(i);
-        }
+    vector<int> bfs(map<int, set<int>>& graph, int start, const set<int>& k) {
         vector<int> ans;
-        if (setK.find(start) != setK.end()){
+        if (k.count(start)) {
             ans.push_back(start);
             return ans;
         }
         queue<pair<int, vector<int>>> q;
-        q.push(make_pair(start, vector<int>{start}));
-        while (!q.empty()){
-            auto p = q.front();
+        q.push({ start, {start} });
+        while (!q.empty()) {
+            auto [v, path] = q.front();
             q.pop();
-            int v = p.first;
-            vector<int> path = p.second;
-            for (auto &next: graph[v]){
-                if (setK.find(next) == setK.end() && find(path.begin(), path.end(), next) == path.end()){
+            for (auto& next : graph[v]) {
+                if (k.count(next) == 0 && find(path.begin(), path.end(), next) == path.end()) {
                     vector<int> newPath = path;
                     newPath.push_back(next);
-                    q.push(make_pair(next, newPath));
+                    q.push({ next, newPath });
                 }
-                else if (setK.find(next) != setK.end()){
+                else if (k.count(next) != 0) {
                     vector<int> newPath = path;
                     newPath.push_back(next);
                     ans = newPath;
@@ -37,38 +31,33 @@ public:
         return ans;
     }
 
-    map<int, set<int>> tograph(vector<vector<int>> &m){
+    map<int, set<int>> tograph(const vector<vector<int>>& m) {
         map<int, set<int>> graph;
-        for (auto &i: m){
+        for (auto& i : m) {
             graph[i[0]].insert(i[1]);
             graph[i[1]].insert(i[0]);
         }
         return graph;
     }
-    vector<int> findexits(int n, vector<vector<int>> &m, vector<int> &k){
+
+    vector<int> findexits(int n, const vector<vector<int>>& m, const set<int>& k) {
         map<int, set<int>> graph = tograph(m);
         vector<int> ans;
-        for (int start = 1; start <= graph.size(); start++){
-                auto path = bfs(graph, start, k);
-                ans.push_back(path.size() - 1);
-            }
+        for (int start = 1; start <= graph.size(); start++) {
+            auto path = bfs(graph, start, k);
+            ans.push_back(path.size() - 1);
+        }
         return ans;
     }
 };
-/*
-n = 10
-m = [(6, 7), (7,5),(5,8),(8,1),(1,10),(10,3),(3,4),(4,9),(9,2)]
-k = (10,8)
 
-*/
-
-int main(){
+int main() {
     int n = 10;
-    vector<vector<int>> m = {{6, 7}, {7,5},{5,8},{8,1},{1,10},{10,3},{3,4},{4,9},{9,2}};
-    vector<int> k = {10,8};
+    vector<vector<int>> m = { {6, 7}, {7,5},{5,8},{8,1},{1,10},{10,3},{3,4},{4,9},{9,2} };
+    set<int> k = { 10,8 };
     Solution s;
     vector<int> ans = s.findexits(n, m, k);
-    for (auto &i: ans){
+    for (auto& i : ans) {
         cout << i << " ";
     }
     cout << endl;
